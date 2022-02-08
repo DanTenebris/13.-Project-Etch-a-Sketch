b70525a6d24f8b6e6b2a64426ac623db3962e586
Daniel <dan.tenebris@hotmail.com>
2022-02-08 16:01:44 -0300

    The buttons for Erase and Clear have been added.

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,20 +1,85 @@
 const body = document.querySelector('body');
 
+const divButton = document.createElement('div');
+divButton.setAttribute('class', 'divButton');
+body.appendChild(divButton);
+
 const unitButton = document.createElement('button');
-unitButton.textContent = 'Number of unities squared';
-body.appendChild(unitButton);
+unitButton.textContent = 'Number of squares';
+unitButton.onclick = unitButtonPrompt;
+divButton.appendChild(unitButton);
+
+const eraseButton = document.createElement('button');
+eraseButton.textContent = 'Erase';
+eraseButton.setAttribute('id', 'erase');
+eraseButton.onclick = erasingButtonFunction;
+divButton.appendChild(eraseButton);
+
+const drawButton = document.createElement('button');
+drawButton.textContent = 'Draw';
+drawButton.setAttribute('id', 'draw');
+drawButton.onclick = drawingButtonFunction;
+
+const clearButton = document.createElement('button');
+clearButton.textContent = 'Clear';
+clearButton.onclick = clearingButtonFunction;
+divButton.appendChild(clearButton);
 
 const mainContainer = document.createElement('main');
 mainContainer.style.display = 'grid';
 body.appendChild(mainContainer);
 
 containerMaker(16);
+divDrawingListener();
 
-//Button in action.
-unitButton.addEventListener('click', () => {
+
+function unitButtonPrompt() {
   const gridQuantity = checkPrompt(showPrompt());
-  containerMaker(gridQuantity);
-});
+  if (!(gridQuantity === undefined)) {
+    containerMaker(gridQuantity);
+    divDrawingListener();
+    changeButton(true);
+  }
+  return;
+}
+
+function erasingButtonFunction() {
+  divErasingListener();
+  changeButton();
+  return;
+}
+
+function drawingButtonFunction() {
+  divDrawingListener();
+  changeButton();
+  return;
+}
+
+function clearingButtonFunction() {
+  const containerGridDiv = document.querySelectorAll('.gridDiv');
+  containerGridDiv.forEach((div) => {
+    erasingHover(div);
+  });
+  return;
+}
+
+function changeButton(option = false) {
+  const buttons = document.querySelectorAll('button');
+  if (!option) {
+    divButton.removeChild(buttons[1]);
+    if (buttons[1].id === 'erase') {
+      divButton.insertBefore(drawButton, buttons[2]);
+    } else {
+      divButton.insertBefore(eraseButton, buttons[2]);
+    }
+  } else if (option) {
+    if (buttons[1].id === 'draw') {
+      divButton.removeChild(buttons[1]);
+      divButton.insertBefore(eraseButton, buttons[2]);
+    }
+  }
+  return;
+}
 
 function deleteContent() {
   while (mainContainer.firstChild) {
@@ -43,6 +108,7 @@ function checkPrompt(value) {
 function containerMaker(quantity) {
   const gridTotal = quantity * quantity;
   const containerBackgroundColor = checkRandomRGB(randomRGB(), randomRGB(), randomRGB());
+  mainContainer.style.backgroundColor = containerBackgroundColor;
 
   for (let i = 0; i < gridTotal; i++) {
     let divGrid = document.createElement('div');
@@ -52,7 +118,6 @@ function containerMaker(quantity) {
   }
 
   addingGridTemplate(quantity);
-  divListener();
   return;
 }
 
@@ -79,18 +144,31 @@ function checkRandomRGB(red, green, blue) {
   }
 }
 
-//Event Listener mouseover
-function divListener() {
+function divDrawingListener() {
   const containerGridDiv = document.querySelectorAll('.gridDiv');
   containerGridDiv.forEach((div) => {
-    div.addEventListener('mouseover', () => {
-      changeColor(div);
+    const newDiv = div.cloneNode(true);
+    div.parentNode.replaceChild(newDiv, div);
+
+    newDiv.addEventListener('mouseover', () => {
+      drawingHover(newDiv);
     });
   });
 }
 
-//This function changes the color when the mouse is over the element.
-function changeColor(divElement) {
+function divErasingListener() {
+  const containerGridDiv = document.querySelectorAll('.gridDiv');
+  containerGridDiv.forEach((div) => {
+    const newDiv = div.cloneNode(true);
+    div.parentNode.replaceChild(newDiv, div);
+
+    newDiv.addEventListener('mouseover', () => {
+      erasingHover(newDiv);
+    });
+  });
+}
+
+function drawingHover(divElement) {
   const divBackgroundColor = getComputedStyle(divElement).backgroundColor;
 
   let rgbArray = divBackgroundColor.match(/(\d[\d\.]*)/g); //Search all the numbers in an array.
@@ -98,6 +176,18 @@ function changeColor(divElement) {
     return parseInt(x);
   });
   rgbArray = rgbArray.map(reduceNumberRgb);
+
+  divElement.style.backgroundColor = 'rgb(' + rgbArray[0] + ', ' + rgbArray[1] + ', ' + rgbArray[2] + ')';
+  return;
+}
+
+function erasingHover(divElement) {
+  const divContainerBackgroundColor = getComputedStyle(mainContainer).backgroundColor;
+
+  let rgbArray = divContainerBackgroundColor.match(/(\d[\d\.]*)/g); //Search all the numbers in an array.
+  rgbArray = rgbArray.map(function(x) {
+    return parseInt(x);
+  });
 
   divElement.style.backgroundColor = 'rgb(' + rgbArray[0] + ', ' + rgbArray[1] + ', ' + rgbArray[2] + ')';
   return;
